95c3d051cd4778134184fa9f61b7126bf4b16eb3
Prasanth <Prasanth08.ios@gmail.com>
2025-07-11 17:12:03 +0530

    Initial Commit

---
+++ ZuperTask-Prasanth/ZuperTask_PrasanthApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZuperTask_PrasanthApp.swift
+//  ZuperTask-Prasanth
+//
+//  Created by Prasanth S on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ZuperTask_PrasanthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
